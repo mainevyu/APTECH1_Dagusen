11e4641c26ff4d393315917d961727379ca6cbbb
Charmaine D. Dagusen <20255342@s.ubaguio.edu>
2026-02-02 08:55:39 +0800

    Update vite.config.ts

--- unit1_lesson1_a_dagusen/vite.config.ts
+++ unit1_lesson1_a_dagusen/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/APTECH1_Dagusen/'
+  base: '/unit1_lesson1_a_dagusen/'
   plugins: [react()],
 })
