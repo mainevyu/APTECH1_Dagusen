797dbeb5fc94725b83506294570737851618cfa0
dagusen_c <dagusen_c@f112.ub.lan>
2026-02-02 08:27:31 +0800

    Initial commit

---
+++ unit1_lesson1_a_dagusen/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+  <> 
+    <h1>Charmaine Danes Dagusen</h1> 
+    <p>_________________________________________________________________</p>
+    <h2>Personal Information</h2>
+    <p><b>Address:</b> #245-A Loakan Liwanag Baguio City</p>
+    <p><b>Email:</b> charmainedanesdagusen15@gmail.com</p> 
+    <p><b>Contact No.:</b> 09085684425</p> 
+    <p><b>Birthdate:</b> March 15, 2006</p>
+    <p>_________________________________________________________________</p>
+    <h2>Education</h2> 
+    <p>Science, Technology, Engineering and Mathematics (STEM)</p>
+    <p>Bachelor of Science in Information Technology</p> 
+    <p>_________________________________________________________________</p>
+    <h2>Awards and Recognition</h2> 
+    <p>Grade 12 With High Honors (2024)</p> 
+    <p>Grade 11 With High Honors (2023) </p> 
+    <p>Supreme Student Government (2021-2023) </p>
+    <p>Filipino School Journalist (2020-2024) </p>
+    <p>Best in Inquiries, Investigations, and Immersion (III) Research</p>
+  </> 
+  );
+}
+
+export default App;
